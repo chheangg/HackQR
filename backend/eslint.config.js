6821618dcecb1b2b7c255125d20b0d93781e0353
chheangg <balldremz@gmail.com>
2024-09-17 14:00:26 -0700

    initialize project with NestJS as backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.stylistic],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      react,
+      '@stylistic': stylistic
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      'indent': ['error', 2],
+      'semi': ['error'],
+      'comma-dangle': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      "react/prop-types": 0,
+    },
+  },
+)
